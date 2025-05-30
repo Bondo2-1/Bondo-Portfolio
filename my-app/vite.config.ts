e7cdf889f42faa8210a7b230eba41a813e81fd61
Mohamed Khaled <85366709+Bondo2-1@users.noreply.github.com>
2025-05-30 23:18:03 +0300

    commit changes for deploy

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -5,6 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "/Bondo-Portfolio
-",
+  base: "/Bondo-Portfolio/",
 });
